3db2c28f4bde95c660f02e78f1b53b0dea6106ce
Marwan Ait Addi-Russier <aitaddi.marwan@gmail.com>
2026-01-29 15:02:03 +0100

    feat : script to create graphs inside an Arango database from a JSON configuration file.

---
+++ src/graphCreator.py
@@ -0,0 +1,37 @@
+
+import sys
+from thesaurusCreator import get_config
+from arango import ArangoClient, database, graph
+
+def create_graph(db : database.StandardDatabase, graph_list : list) -> None:
+    """Creates the desired graph from a JSON configuration file stored as a dict"""
+    for g in graph_list:
+        if db.has_graph(g["name"]):
+            db.delete_graph(g["name"])
+
+        curr_graph = db.create_graph(g["name"])
+
+        #Creating the edge definitions
+        for definition in g["relations"]:
+            print(definition)
+            curr_graph.create_edge_definition(
+                edge_collection = definition["edge_collection"],
+                from_vertex_collections = definition["from_vertex_collections"],
+                to_vertex_collections = definition["to_vertex_collections"]
+            )
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Please provide config file path as first argument")
+        sys.exit()
+
+    config : dict = get_config(sys.argv[1], "config/graph-config-schema.json")
+    credentials : dict = config["credentials"]
+    graphs : list = config["graphs"]
+
+    client : ArangoClient = ArangoClient(hosts=credentials['host'])
+    curr_db : database.StandardDatabase = client.db(credentials['database'], username=credentials['username'], password=credentials['password'])
+
+    print(graphs)
+
+    create_graph(curr_db, graphs)
