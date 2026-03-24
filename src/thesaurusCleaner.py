832520122592c213e31153dc483134a7ded2023b
Marwan-Ru <marwan.aitaddi+github@sans-nuage.fr>
2026-03-24 09:46:16 +0100

    fix : Unnecessary deleting in document collection cleanup

--- src/thesaurusCleaner.py
+++ src/thesaurusCleaner.py
@@ -5,48 +5,36 @@ It will check for invalid relations and properties, to reduce friction in the us
 
 from arango import database, exceptions, ArangoClient
 
-def cleanup_document_collection(db : database.StandardDatabase, collection_name : str):
-    """
-    TODO : Docstring
-    Cleans all the duplicates in a collection with the given collection name
-    """
-
-    if not db.has_collection(collection_name):
-        print(f"Collection {collection_name} does not exist")
-    else:
-        if not db.collection(collection_name).properties()['edge'] :
-            db.aql.execute("\
-                FOR doc IN @@collection \
-                    COLLECT myid = doc.ark INTO keys = doc._key \
-                    LET allButFirst = SLICE(keys, 1) \
-                    FOR k IN allButFirst \
-                        REMOVE k IN @@collection",
-                bind_vars={"@collection" : collection_name})
-        else:
-            print(f"{collection_name} is an edge collection, cannot perform this type of cleanup")
-
 def cleanup_edge_collection(db : database.StandardDatabase, collection_name : str):
     """
     TODO : Docstring
     """
+    to_delete = []
+
     if not db.has_collection(collection_name):
         print(f"Collection {collection_name} does not exist")
     else:
         if db.collection(collection_name).properties()['edge'] :
-
+            print("Fetching...")
             #Now we want to test every document in the edges to see if they exist
             #It feels very trashy tho
-            for edge in db.collection(collection_name).all() :
+            all = db.collection(collection_name).all()
+            print("Now checking...")
+            for edge in all :
                 try :
                     _to = db.document(edge['_to'])
                     _from = db.document(edge['_from'])
                     if _to is None or _from is None:
-                        db.collection(collection_name).delete(edge)
+                        to_delete.append(edge)
 
                 except (exceptions.DocumentGetError, exceptions.DocumentRevisionError) as e:
-                    db.collection(collection_name).delete(edge)
+                    to_delete.append(edge)
         else:
             print(f"{collection_name} is not an edge collection, cannot perform this type of cleanup")
+
+        print("Deleting now...")
+
+        db.collection(collection_name).delete_many(to_delete)
 
 
     return
@@ -62,12 +50,10 @@ def cleanup_database(host: str, db_name: str, user: str, password: str):
         if not coll['system']:
             if db.collection(coll['name']).properties()['edge']:
                 cleanup_edge_collection(db, coll['name'])
-            else:
-                cleanup_document_collection(db, coll['name'])
 
 if __name__ == "__main__":
     testclient = ArangoClient("http://localhost:8529")
 
     testdatabase = testclient.db("TEATIME","root","test")
 
-    cleanup_edge_collection(testdatabase, "th15_relations")
+    cleanup_edge_collection(testdatabase, "intraTheso_relations")
