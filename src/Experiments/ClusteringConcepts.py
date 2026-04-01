12f3f37a96cae3b19b45a6307bbcd1df668125ac
Marwan-Ru <marwan.aitaddi+github@sans-nuage.fr>
2026-04-01 13:53:25 +0200

    experiment : Retrieving cluster and associated concepts for further visualisation

--- src/Experiments/ClusteringConcepts.py
+++ src/Experiments/ClusteringConcepts.py
@@ -2,11 +2,12 @@
 Hierarchical clustering on concepts using a precomputed distance matrix
 """
 import numpy as np
+from arango import ArangoClient, database
 from sklearn.cluster import AgglomerativeClustering
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
-def plot_dendrogram(model, **kwargs):
+def create_linkage_matrix(model) -> np.ndarray:
     # Create linkage matrix and then plot the dendrogram
 
     # create the counts of samples under each node
@@ -25,43 +26,67 @@ def plot_dendrogram(model, **kwargs):
         [model.children_, model.distances_, counts]
     ).astype(float)
 
+    return linkage_matrix
+
+def plot_dendrogram(model, **kwargs):
+    linkage_matrix = create_linkage_matrix(model)
+
     # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
 
+def fetch_distance_matrix(file_path : str) -> tuple[list ,np.ndarray]:
+    # Rebuilding the matrix from a csv
+    with open(file_path, 'r') as f:
+        ids = f.readline()
+        ids = ids.split(',')
+        size = len(ids)
 
-# Rebuilding the matrix from a csv
-with open("distance_matrix_th15_graph.csv", 'r') as f:
-    ids = f.readline()
-    ids = ids.split(',')
-    size = len(ids)
+        matrix : list = []
 
-    matrix : list = []
-
-    for i in range(size):
-        line = [int(d) for d in f.readline().split(',')]
-        matrix.append(line[:size])
+        for i in range(size):
+            line = [int(d) for d in f.readline().split(',')]
+            matrix.append(line[:size])
 
 
-"""linkage_matrix = linkage(matrix, method='average')
+        return ids, np.asarray(matrix)
 
-# Extract clusters at different levels by specifying the number of clusters
-clusters_2 = fcluster(linkage_matrix, 2, criterion='maxclust')
-clusters_8 = fcluster(linkage_matrix, 8, criterion='maxclust')
 
-# Count points in each cluster for both clusterings
-cluster_counts_2 = np.unique(clusters_2, return_counts=True)
-cluster_counts_4 = np.unique(clusters_8, return_counts=True)
+if __name__ == "__main__":
+    ids, matrix = fetch_distance_matrix("distance_matrix_th15_graph.csv")
 
-print(cluster_counts_2)
-print(cluster_counts_4)
+    clustering = AgglomerativeClustering(metric="precomputed", linkage="average", distance_threshold=0, n_clusters=None)
+
+    clustering = clustering.fit(matrix)
+
+    linkage_matrix = create_linkage_matrix(clustering)
+
+    nb_clusters = 8
+
+    # These create a list, where clusters_X[i] returns the cluster number of item i in the original ids list
+    clusters = fcluster(linkage_matrix, nb_clusters, criterion='maxclust')
+
+    cluster_with_ids = [[] for _ in range(nb_clusters)]
+
+    for i in range(len(ids)):
+        cluster_with_ids[clusters[i] - 1].append(ids[i])
+        cluster_with_ids[clusters[i] - 1].append(ids[i])
+
+    print(len(cluster_with_ids[1]))
+
+    client = ArangoClient(hosts="http://localhost:8529")
+    db: database.StandardDatabase = client.db("TEATIME", username="root", password="test")
+    thesaurus_documents: database.StandardCollection = db.collection("th15")
+
+    for cluster in cluster_with_ids:
+        test = thesaurus_documents.get_many(cluster)
+        test = [doc['name'] for doc in test]
+
+        print(len(test))
+        print(test)
+
 """
-
-clustering = AgglomerativeClustering(metric="precomputed", linkage="average", distance_threshold=0, n_clusters=None)
-
-clustering = clustering.fit(matrix)
-
 plt.title("Hierarchical Clustering Dendrogram")
 # plot the top three levels of the dendrogram
 plot_dendrogram(clustering, truncate_mode="level", p=4)
 plt.xlabel("Number of points in node (or index of point if no parenthesis).")
-plt.show()
+plt.show()"""
