bc7d1fbc3c3f7b6e814a17e1b6b1d0b9fc355602
Marwan-Ru <marwan.aitaddi+github@sans-nuage.fr>
2026-05-05 22:10:09 +0200

    feature : Dockerisable Arango Importer

--- src/ArangoImporter/entrypoint.sh
+++ src/ArangoImporter/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-CMD="python3 main.py"
+
+CMD="python3 TEATIME_PhD_Experiments/src/ArangoImporter/main.py"
 
 if [ -n "${THESO_CONFIG}" ]; then
   CMD="$CMD --thesaurus-config ${THESO_CONFIG}"
+fi
 
 if [ -n "${GRAPH_CONFIG}" ]; then
   CMD="$CMD --graph_config ${GRAPH_CONFIG}"
@@ -11,5 +13,6 @@ fi
 
 if [ -n "${DUMP_PATH}" ]; then
   CMD="$CMD --dump-path ${DUMP_PATH}"
+fi
 
 exec $CMD
